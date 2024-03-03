4cf634efa67b4cde1044ab92a9149ccbf83fa741
fayiznoorudheen <fayisnoorudheen@gmail.com>
2024-03-03 11:44:07 +0530

    feat(main screen):screenUnitInit package added and other changes added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:text/image_uploading/image_uploading_screen.dart';
 import 'package:text/splash/splash_screen.dart';
-
-
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: SplashScreen(),
-  ),);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      splitScreenMode: true,
+      minTextAdapt: true,
+      designSize: const Size(360, 690),
+      builder: (ctx, _) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const ImageUploadingScreen()),
+    );
+  }
 }
