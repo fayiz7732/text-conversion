494a58251b615fa793edfbf491446685bfad63f6
fayiznoorudheen <fayisnoorudheen@gmail.com>
2024-03-03 20:39:41 +0530

    feat(main screen):added new packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:text/image_uploading/image_uploading_screen.dart';
+import 'package:text/login/login_screen.dart';
+import 'package:text/register/registartion_screen.dart';
 import 'package:text/splash/splash_screen.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -21,7 +27,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          home: const ImageUploadingScreen()),
+          home: LoginScreen()),
     );
   }
 }
